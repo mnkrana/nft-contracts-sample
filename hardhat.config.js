f9a67764d616f3a99affef84fce04eb1c946a69c
Mayank <mnk.rana@gmail.com>
2025-03-18 09:50:28 +0530

    feat(packages): add packages to deploy

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,49 @@
-/** @type import('hardhat/config').HardhatUserConfig */
+require("@nomicfoundation/hardhat-verify");
+require("hardhat-deploy");
+require("dotenv").config();
+require("@solarity/hardhat-gobind");
+
+const { PRIVATE_KEY, PUPPY_RPC_URL, ETHERSCAN_API_KEY } =
+  process.env;
+
+console.log("Loaded Private Key:", PRIVATE_KEY);
+
 module.exports = {
-  solidity: "0.8.28",
+  solidity: {
+    version: "0.8.25",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+      1: 0,
+    },
+  },
+  networks: {
+    puppy: {
+      url: PUPPY_RPC_URL,
+      chainId: 157,
+      accounts: PRIVATE_KEY ? [PRIVATE_KEY] : [],
+    }
+  },
+  etherscan: {
+    apiKey: {
+      puppy: ETHERSCAN_API_KEY || "empty",
+    },
+    customChains: [
+      {
+        network: "puppy",
+        chainId: 157,
+        urls: {
+          apiURL: "https://puppyscan.shib.io/api",
+          browserURL: "https://puppyscan.shib.io",
+        },
+      }
+    ],
+  },
 };
